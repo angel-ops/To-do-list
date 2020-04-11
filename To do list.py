e063685745b6c09c477a352f08470b2ec8eb6d21
Ángel Martínez Ruiz <angel.8296@hotmail.com>
2020-04-10 23:24:29 -0500

    To do

--- To do list.py
+++ To do list.py
@@ -1,7 +1,12 @@
 #To Do List
 
 #import django
- 
+ from django.db import models
+
+ class Todo(models.Model):
+     added_date = models.DateTimeField()
+     text = models.CharField(max_legth = 200)
+     
 #load
 #get to do items
 #show to do items
